fce8c29d303e200e33fc5cef570a007809dbcdc2
zinoad62-web <zinoad62@gmail.com>
2026-08-16 23:21:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def keep_alive():
 
 
 # --- كود البوت الخاص بك ---
-TOKEN = 'bot = TeleBot("8932809251:AAExxj0ORQhI_tFWY6wsbzmjJYgtlegNb_o")
-'
+bot = TeleBot("8932809251:AAExxj0ORQhI_tFWY6wsbzmjJYgtlegNb_o")
+
 bot = telebot.TeleBot(TOKEN)
 
 
